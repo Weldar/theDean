6aee52f1d930a68a8136ea6e62193ffa0f9aa969
Weldar <2402337-Weldar@users.noreply.gitlab.com>
2025-02-04 23:00:48 -0500

    Changed logs to console warns.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = process.env.BOT_TOKEN;
+const token = process.env.BOT_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -12,25 +12,25 @@ client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandsFolders) {
+commandFolders.forEach(folder => {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
+    
+    commandFiles.forEach(file => {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
 
-        if('data' in command && 'execute' in command) {
+        if (command.data && command.execute) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
-    }
-}
+    });
+});
+
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 // Log in to Discord with your client's token
